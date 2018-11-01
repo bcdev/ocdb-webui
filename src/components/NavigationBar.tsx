f8512ed6a783de2345715e932eae6e1d359712ee
hans-permana <hans.permana@gmail.com>
2018-11-01 16:20:15 +0100

    use manual navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Alignment, Navbar, Tab, Tabs} from '@blueprintjs/core';
+import {Tab, Tabs} from '@blueprintjs/core';
 import logo from './eumetsat.png';
 import './NavigationBar.css';
 
@@ -22,11 +22,11 @@ export class NavigationBar extends React.PureComponent<NavigationBarProps, Navig
 
     public render() {
         return (
-            <Navbar className='bp3-dark'>
-                <Navbar.Group align={Alignment.LEFT}>
+            <div className='nav-container'>
+                <div className='nav-img'>
                     <img src={logo} alt="logo" height='40px' width='125px'/>
-                </Navbar.Group>
-                <Navbar.Group className='nav-tab-group'>
+                </div>
+                <div className='nav-tabs'>
                     <Tabs
                         animate={this.state.animate}
                         id="navbarTabs"
@@ -40,11 +40,11 @@ export class NavigationBar extends React.PureComponent<NavigationBarProps, Navig
                         <Tab id="UploadData" title="Upload Data"/>
                         <Tab id="Wiki" title="Wiki"/>
                     </Tabs>
-                </Navbar.Group>
-                <Navbar.Group className='nav-right-group' align={Alignment.RIGHT}>
-                    <input className="bp3-input nav-right-group-search" type="search" placeholder="Search"/>
-                </Navbar.Group>
-            </Navbar>
+                </div>
+                <div className='nav-search'>
+                    <input className="bp3-input" type="search" placeholder="Search"/>
+                </div>
+            </div>
         )
     }
 }
