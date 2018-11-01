e860c161e9b9b49bea565fa6f1cee38cc5007bb7
hans-permana <hans.permana@gmail.com>
2018-11-01 18:44:04 +0100

    draft file search panel

--- src/components/FileSearchPanel.tsx
+++ src/components/FileSearchPanel.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import './FileSearchPanel.css';
+import {Button, Collapse, Icon} from '@blueprintjs/core';
 
 export class FileSearchPanel extends React.PureComponent<any, any> {
 
     public render() {
         return (
             <div className='file-search-panel'>
-                <span>File Search</span>
+                <div className='file-search-panel-info'>The File Search allows visitors to search the bio-optical
+                    archive for in situ measurements of apparent and inherent optical properties, phytoplankton pigment
+                    concentrations, and other oceanographic and atmospheric data. Search results return a list of
+                    matching data files which may be viewed, downloaded, mapped and plotted. Data access and use are
+                    governed by the SeaBASS Data Access Policy.
+                </div>
+                <div>
+                    <div className='collapsible-title'>
+                        <div>General Search Parameters</div>
+                        <Icon icon='chevron-down' className='collapsible-title-icon'/>
+                    </div>
+                    <Collapse isOpen={true} className='general-search-collapsible'>
+                        <div>Date input</div>
+                        <div>Map display</div>
+                        <div>Keyword search filters</div>
+                    </Collapse>
+                </div>
+                <div>
+                    <div className='collapsible-title'>
+                        <div>File Search Parameters</div>
+                        <Icon icon='chevron-down' className='collapsible-title-icon'/>
+                    </div>
+                    <Collapse isOpen={true} className='file-search-collapsible'>
+                        <div>Water depth</div>
+                        <div>Wavelength options</div>
+                        <div>Include Optically Shallow Measurements</div>
+                        <div>Products</div>
+                    </Collapse>
+                </div>
+                <Button icon="search" text='Search'/>
             </div>
         )
     }
